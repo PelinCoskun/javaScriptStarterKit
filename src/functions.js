80d235b05744aeca5c6d12c04b1158ebddaff53e
PelinCoskun <pelinefpit@gmail.com>
2024-02-12 18:58:16 +0300

    kjh

--- src/functions.js
+++ src/functions.js
@@ -62,7 +62,13 @@ let products=[
 
 addToCart4(products)
 
-function add(number1,number2) {
-    console.log(number1+number2)
+function add(...numbers) {
+    let total=0
+    for (let i = 0; i < numbers.length; i++) {
+        total=total+numbers[i]
+    }
+    console.log(total)
 }
 add(20,30)
+add(20,30,40)
+add(20,30,40,50)
