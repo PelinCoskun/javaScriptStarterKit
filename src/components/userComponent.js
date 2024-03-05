fcc29b3e472a62222e5ae587473d023edf9c4f23
PelinCoskun <pelinefpit@gmail.com>
2024-03-05 10:43:17 +0300

    sd

---
+++ src/components/userComponent.js
@@ -0,0 +1,1 @@
+console.log("User component yüklendi")
---
+++ src/services/userService.js
@@ -0,0 +1,13 @@
+export default class UserService{
+    add(){
+        console.log("kullanıcı eklendi")
+    }
+
+    list(){
+        console.log("kullanıcı listelendi")
+    }
+
+    getById(id){
+        console.log("kullanıcı eklendi")
+    }
+}
