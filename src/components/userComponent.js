801bf99fcbf9715576b53254730900015c0fc605
PelinCoskun <pelinefpit@gmail.com>
2024-03-05 18:07:54 +0300

    sdg

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,19 +1,21 @@
+import { MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
 console.log("User component yüklendi")
 
-let userService = new UserService
+let logger1=new MongoLogger()
+let userService = new UserService(logger1)
 
 
 let user1 = new User(1, "Pelin", "Coskun", "İstanbul")
-let user2 = new User(1, "İzel", "Coskun", "İstanbul")
+let user2 = new User(2, "İzel", "Coskun", "İstanbul")
 userService.add(user1)
 userService.add(user2)
 
+console.log(userService.list())
+console.log(userService.getById(2))
 
-userService.getById(1)
-userService.list()
 
 
 
