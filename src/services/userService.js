be727d0ba0b58293d6002f8283c3791cdd91e671
PelinCoskun <pelinefpit@gmail.com>
2024-03-08 11:09:21 +0300

    dh

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,19 @@
+import {users} from "../data/users.js"
+
 export default class UserService {
     constructor(loggerService){
-        this.users=[]
+        this.employees=[]
+        this.customers=[]
         this.loggerService=loggerService
     }
+
+load(){
+    for (const user of users) {
+            console.log(user)
+        }
+    }
+}
+
     add(user) {
         this.users.push(user)
         this.loggerService.log(user)
