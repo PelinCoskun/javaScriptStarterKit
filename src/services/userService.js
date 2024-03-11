e4fa86f2e9c2ff480a4c5f622c73c49154aee894
PelinCoskun <pelinefpit@gmail.com>
2024-03-11 15:33:15 +0300

    end

--- src/services/userService.js
+++ src/services/userService.js
@@ -93,11 +93,11 @@ export default class UserService {
     getCustomersSorted() {
      return this.customers.sort((customer1, customer2) => {
             if (customer1.firstName < customer2.firstName) {
-                return 1
+                return -1
             } else if (customer1.firstName === customer2.firstName) {
                 return 0
             } else {
-                return -1
+                return 1
             }
         })
     }
