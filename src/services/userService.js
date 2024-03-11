c7610e26119ab6a478a6c9513638d3acd988cb42
PelinCoskun <pelinefpit@gmail.com>
2024-03-11 12:36:32 +0300

    jhkb

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,29 +1,67 @@
-import {users} from "../data/users.js"
+import { users } from "../data/users.js"
+import DataError from "../models/dataError.js"
 
 export default class UserService {
-    constructor(loggerService){
-        this.employees=[]
-        this.customers=[]
-        this.loggerService=loggerService
+    constructor(loggerService) {
+        this.employees = []
+        this.customers = []
+        this.errors = []
+        this.loggerService = loggerService
     }
 
-load(){
-    for (const user of users) {
-            console.log(user)
+    load() {
+        for (const user of users) {
+            switch (user.type) {
+                case "customer":
+                    if(!this.checkCustomerValidityForErors(user)){
+                        this.customers.push(user)
+                    }
+                    break;
+                case "employee":
+                    this.employees.push(user)
+                    break;
+                default:
+                    this.errors.push(new DataError("wrong user type",user))
+                    break;
+            }
         }
     }
+
+
+checkCustomerValidityForErors(user){
+    let reguiredFields="id firstName lastName age city".split(" ")
+    let hasErrors=false
+    for (const field of reguiredFields) {
+        if (!user[field]) {
+            hasErrors=true
+            this.errors.push(new DataError(`Validation problem. ${field} is required`,user))
+        }
+    }
+    return hasErrors
+}
+
+checkEmployeeValidityForErors(user){
+    let reguiredFields="id firstName lastName age city salary".split(" ")
+    let hasErrors=false
+    for (const field of reguiredFields) {
+        if (!user[field]) {
+            hasErrors=true
+            this.errors.push(new DataError(`Validation problem. ${field} is required`,user))
+        }
+    }
+    return hasErrors
 }
 
     add(user) {
-        this.users.push(user)
+        //  this.users.push(user)
         this.loggerService.log(user)
     }
 
     list() {
-        return this.users
+        // return this.users
     }
 
     getById(id) {
-       return this.users.find(u=>u.id ===id)
+        //  return this.users.find(u=>u.id ===id)
     }
 }
